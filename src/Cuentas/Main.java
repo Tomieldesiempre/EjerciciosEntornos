ef4ecaad5edc5ff8412e127aaf78aa284d9cee26
Tomas <Tomas@DESKTOP-PUV4F4D>
2021-03-09 12:25:36 +0100

    Esta es la primera subida

---
+++ src/Cuentas/Main.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Cuentas;
+
+/**
+ *
+ * @author Tomas
+ */
+public class Main {
+    
+    public static void main(String[] args){
+        
+        OperativaCuenta();
+        
+    }
+
+    private static void OperativaCuenta() {
+        CCuenta cuenta1;
+        double saldoActual;
+        
+        cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
+        
+        saldoActual = cuenta1.estado();
+        
+        System.out.println("El saldo actual es " + saldoActual );
+        
+        try{
+            
+            cuenta1.retirar(2300);
+            
+        }catch(Exception e){
+            
+            System.out.println("Fallo al retirar");
+            
+        }
+        
+        try{
+            
+            System.out.println("Ingreso en cuenta");
+            cuenta1.ingresar(695);
+            
+        }catch(Exception e){
+            
+            System.out.println("Fallo al ingresar");
+            
+        }
+        
+        System.out.println("Primera subida");
+        
+    }
+    
+}
